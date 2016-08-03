83527e32b63e7943dbc8e6cfbd5b3aa154b2fb27
MartkCz <antik494@gmail.com>
2016-08-03 17:44:29 +0200

    FrontFactory extends WebChemistry\Forms\Factory\FormFactory

--- app/other/factories/forms/FrontFactory.php
+++ app/other/factories/forms/FrontFactory.php
@@ -5,59 +5,31 @@ namespace App\Factories\Forms;
 use App\Renderers\Forms\FrontRenderer;
 use Kdyby\Translation\Translator;
 use Nette\Application\UI\ITemplateFactory;
-use Nette\Object;
+use Nette\Localization\ITranslator;
 use WebChemistry\Forms\Doctrine;
+use WebChemistry\Forms\Factory\FormFactory;
 use WebChemistry\Forms\Factory\IFormFactory;
 use WebChemistry\Forms\Form;
 
-class FrontFactory extends Object implements IFormFactory {
-
-	/** @var Doctrine */
-	private $doctrine;
-
-	/** @var array */
-	private $parameters;
+class FrontFactory extends FormFactory implements IFormFactory {
 
 	/** @var Translator */
 	private $translator;
 
-	/** @var ITemplateFactory */
-	private $templateFactory;
 
-	/**
-	 * @param Doctrine $doctrine
-	 * @param Translator $translator
-	 */
-	public function __construct(Doctrine $doctrine, Translator $translator = NULL, ITemplateFactory $templateFactory = NULL) {
-		$this->doctrine = $doctrine;
+	public function __construct(ITemplateFactory $templateFactory = NULL, Doctrine $doctrine = NULL, ITranslator $translator = NULL) {
+		parent::__construct($templateFactory, $doctrine);
 		$this->translator = $translator;
-		$this->templateFactory = $templateFactory;
-	}
-
-	/**
-	 * @param array $parameters
-	 * @return self
-	 */
-	public function setParameters(array $parameters) {
-		$this->parameters = $parameters;
-
-		return $this;
 	}
 
 	/**
 	 * @return Form
 	 */
 	public function create() {
-		$form = new Form;
+		$form = parent::create();
 
+		$form->setTranslator($this->translator);
 		$form->setRenderer(new FrontRenderer());
-		if ($this->translator) {
-			$form->setTranslator($this->translator);
-		}
-		if (isset($this->parameters['recaptcha'])) {
-			$form->setRecaptchaConfig($this->parameters['recaptcha']);
-		}
-		$form->injectComponents($this->doctrine, $this->templateFactory);
 
 		return $form;
 	}
