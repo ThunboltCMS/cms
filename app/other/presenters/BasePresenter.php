11e95f9700fe6a43c022886f8f06356a156b68b4
MartkCz <antik494@gmail.com>
2016-08-05 13:34:06 +0200

    BasePresenter: added trait TSuggestion

--- app/other/presenters/BasePresenter.php
+++ app/other/presenters/BasePresenter.php
@@ -3,7 +3,10 @@
 namespace App\Presenters;
 
 use Thunbolt\Presenters;
+use WebChemistry\Forms\Traits\TSuggestion;
 
 abstract class BasePresenter extends Presenters\BasePresenter {
+
+	use TSuggestion;
 
 }
