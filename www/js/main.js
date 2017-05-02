e1acb3f8c0bf2cd20413272b0a55a8af3876dd47
MartkCz <antik494@gmail.com>
2017-05-02 11:46:14 +0200

    flashes js in main.js

--- www/js/main.js
+++ www/js/main.js
@@ -1,12 +1,30 @@
 'use strict';
 (function ($, undefined) {
+    // nette ajax
     $.nette.init();
 
+    // form errors registration
     Thunbolt.FormErrors.addListenerOnChange();
     Thunbolt.FormErrors.init();
 
+    // form controls registration
     WebChemistry.FormControls.registerNetteAjaxEvent();
     WebChemistry.FormControls.init();
+
+    // document - ready
+    $(document).ready(function () {
+        // flashes hide effect
+        $('.flashes-container .notify').each(function (index) {
+            var time = 6e3 * (index + 1);
+            var that = this;
+
+            setTimeout(function () {
+                $(that).fadeOut(500, function () {
+                    $(this).remove();
+                });
+            }, time);
+        });
+    });
 })(jQuery);
 
 
