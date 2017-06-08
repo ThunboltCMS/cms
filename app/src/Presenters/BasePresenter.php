7044cf4677346232546486ccac095516d026a611
MartkCz <antik494@gmail.com>
2017-06-08 20:48:09 +0200

    BasePresenter: inject EntityManager

--- app/src/Presenters/BasePresenter.php
+++ app/src/Presenters/BasePresenter.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace App\Presenters;
 
+use Doctrine\ORM\EntityManager;
 use Thunbolt\Application\Presenter;
 
 abstract class BasePresenter extends Presenter {
 
+	/** @var EntityManager @inject */
+	public $em;
 
 }
