1efff86bcd1637169d203d8dea75e1c05bfe0439
MartkCz <antik494@gmail.com>
2017-06-08 20:44:27 +0200

    added class UserDAO

---
+++ app/src/Model/UserDAO.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model;
+
+use Doctrine\ORM\EntityManager;
+use Thunbolt\User\Interfaces\IUserDAO;
+use Thunbolt\User\Interfaces\IUserModel;
+use Thunbolt\User\Interfaces\IUserRepository;
+
+class UserDAO implements IUserDAO {
+
+	/** @var EntityManager */
+	private $em;
+
+	public function __construct(EntityManager $em) {
+		$this->em = $em;
+	}
+
+	public function merge(IUserModel $model): void {
+		$this->em->merge($model);
+		$this->em->flush();
+	}
+
+	public function getRepository(): IUserRepository {
+		throw new \Exception('TODO: return IUserRepository'); // Todo: return IUserRepository
+	}
+
+}
