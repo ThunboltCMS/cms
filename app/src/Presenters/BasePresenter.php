7c37adf1032576512f85defe5084d3acdf499ebf
MartkCz <antik494@gmail.com>
2017-07-07 14:25:51 +0200

    BasePresenter: removed $em

--- app/src/Presenters/BasePresenter.php
+++ app/src/Presenters/BasePresenter.php
@@ -4,12 +4,8 @@ declare(strict_types=1);
 
 namespace App\Presenters;
 
-use Doctrine\ORM\EntityManager;
 use Thunbolt\Application\Presenter;
 
 abstract class BasePresenter extends Presenter {
-
-	/** @var EntityManager @inject */
-	public $em;
 
 }
