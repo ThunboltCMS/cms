8c4225a8d0ceb855b56e2ffed033e877a3e5069b
MartkCz <antik494@gmail.com>
2017-07-15 10:42:59 +0200

    development.neon is used only when environment variable NETTE_DEV is set

--- app/mode.php
+++ app/mode.php
@@ -1,4 +1,9 @@
 <?php
 
-$configurator->addConfig(__DIR__ . '/config/environment/development.neon');
-//$configurator->addConfig(__DIR__ . '/config/environment/production.neon');
+$configurator->addConfig(
+	__DIR__ . '/config/environment/' . (getenv('NETTE_DEV') ? 'development.neon' : 'production.neon')
+);
+
+// only if we can't set env variable
+/*$configurator->addConfig(__DIR__ . '/config/environment/development.neon');
+//$configurator->addConfig(__DIR__ . '/config/environment/production.neon');*/
