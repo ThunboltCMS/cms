134234a4f44a8d950b8436d993b680a3e430d537
MartkCz <antik494@gmail.com>
2017-08-15 16:23:36 +0200

    BaseForm: added method flashMessage

--- app/src/Forms/BaseForm.php
+++ app/src/Forms/BaseForm.php
@@ -7,6 +7,7 @@ namespace App\Forms;
 use App\Factories\Forms\IFormFactory;
 use App\UI\Form;
 use Doctrine\ORM\EntityManager;
+use Thunbolt\Components\IFlashes;
 
 abstract class BaseForm {
 
@@ -16,13 +17,21 @@ abstract class BaseForm {
 	/** @var EntityManager */
 	protected $em;
 
-	public function injectComponents(EntityManager $em, IFormFactory $factory): void {
+	/** @var IFlashes */
+	private $flashes;
+
+	public function injectComponents(EntityManager $em, IFormFactory $factory, IFlashes $flashes): void {
 		$this->em = $em;
 		$this->factory = $factory;
+		$this->flashes = $flashes;
 	}
 
 	protected function create(): Form {
 		return $this->factory->create();
+	}
+
+	protected function flashMessage(string $message, string $type = 'success'): \stdClass {
+		return $this->flashes->flashMessage($message, $type);
 	}
 
 }
